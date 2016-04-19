27446bd9ce4d38a243c84f0804352156d87f8ab0
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-19 17:16:07 +0530

    index page
    testing of index page

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
-//session_start();
-//if(!empty($_SESSION["username_admin"]))
-//{
-	//echo "<script>window.location.href='login.php';</script>";
+session_start();
+if(!empty($_SESSION["username_admin"]))
+{
+	echo "<script>window.location.href='login.php';</script>";
 	
-//}else{
-	//echo "<script>window.location.href='login.php';</script>";
-//}
+}else{
+	echo "<script>window.location.href='login.php';</script>";
+}
 
 ?>
