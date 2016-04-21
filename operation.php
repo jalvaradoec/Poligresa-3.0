62a9cb077b0da2d73ea2ef6c54727a2cc63a2c8f
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-21 19:55:37 +0530

    productpopup

--- operation.php
+++ operation.php
@@ -791,7 +791,7 @@ include_once("utils.php");
           <button type="button" class="close" data-dismiss="modal">&times;</button>
           <h4 class="modal-title">Tabla de amortizacion</h4>
         </div>
-		<?php  $sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber INNER JOIN App_Amortization ap ON ac.App_Credits_BankOperNumber = ap.App_Amortization_BankOperation WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+		<?php  $sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber INNER JOIN App_Amortization ap ON ac.App_Credits_BankOperNumber = ap.App_Amortization_BankOperation WHERE  ac.App_Amortization_BankOperation =".$_GET["operno"];
 				$result=mysql_query($sql);
 				$row=mysql_fetch_array($result);
 		?>
@@ -834,7 +834,7 @@ include_once("utils.php");
 				</thead>
                 <tbody>
 				<?php
-			   $sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber INNER JOIN App_Amortization ap ON ac.App_Credits_BankOperNumber = ap.App_Amortization_BankOperation WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+			   $sql="select * from App_Amortization WHERE App_Amortization_BankOperation =".$_GET["operno"];
 				$result=mysql_query($sql);
 				while($row=mysql_fetch_array($result)){
 				?>
