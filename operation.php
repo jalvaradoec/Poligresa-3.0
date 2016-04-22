1e3e47030cc85ac38e812a61c150ae188665d592
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-22 18:29:35 +0530

    activity

--- operation.php
+++ operation.php
@@ -1735,7 +1735,7 @@ var date2=d.getDate();
 var month = d.toLocaleString(locale, { month: "short" });
 var year=d.getFullYear();
 var date1=date2 + '-' + month + '-' + year;
-alert(date1);
+alert(date2);
  //$('#dateselector').val(date1);
 var d1=d.getHours();
 var d2=(d.getMinutes()<10?'0':'') + d.getMinutes();
