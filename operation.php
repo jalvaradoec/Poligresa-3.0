b5ab49c6148c9fc4a9f4fe5e73d5e60b415b42f2
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-23 18:21:34 +0530

    agreement

--- operation.php
+++ operation.php
@@ -1946,7 +1946,7 @@ function ChangeUrl(title, url) {
         alert("Browser does not support HTML5.");
     }
 }
-alert($('.curdebt').val(););
+alert($('.curdebt').val());
 alert($('.interest').val());
 alert($('.shares').val());
 //var monthpayment1=($('.balance2').val() + $('.interest').val()) / $('.shares').val();
