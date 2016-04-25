c183cda0356372328156e399888d147a71bd9a7e
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-25 12:41:49 +0530

    agreement

--- operation.php
+++ operation.php
@@ -1973,10 +1973,9 @@ var Alerter = {
 			//{
 			//$('.monthpayment1').val(monthpayment1.toFixed(2));
 			//}
-			var total=$('.balance2').val()+$('.interest').val();
-			var monthpayment2=5* (total/($('.monthpayment1').val()-1));
+			var monthpayment2=5* ($('.balance2').val()+$('.interest').val()/($('.monthpayment1').val()-1));
 			$('.monthpayment2').val(monthpayment2.toFixed(2));
-			//var lastpayment=total-($('.monthpayment2').val()*($('.monthpayment1').val()-1));
+			//var lastpayment=$('.balance2').val()+$('.interest').val()-($('.monthpayment2').val()*($('.monthpayment1').val()-1));
 			//$('.lastpayment').val(lastpayment.toFixed(2));
 			this.Timer = setTimeout("Alerter.Alert()", this.Wait * 100);
 		}
