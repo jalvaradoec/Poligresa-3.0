df25aaa44f5517c210c749bcc62c0f6128d72d99
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-25 12:23:43 +0530

    agreement

--- operation.php
+++ operation.php
@@ -1954,7 +1954,7 @@ var Alerter = {
 			this.Timer = setTimeout("Alerter.Alert()", this.Wait * 100);
 		},
 		Alert : function(){
-			alert("hello");
+			//alert("hello");
 			var curdebt=$('.curdebt').val();
 			var discount=$('.discount').val();
 			var balance1=curdebt-discount;
