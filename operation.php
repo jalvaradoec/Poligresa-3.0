6bf3efae2312124cda536a696ecb2b0e4536491b
Dhiren <vaghani.dhiren.21@gmail.com>
2016-04-26 15:49:40 +0530

    agreement

--- operation.php
+++ operation.php
@@ -2260,7 +2260,7 @@ if (isset($_POST['create'])) {
 			{
 				$i1=$i-1;
 				$date1 = $_POST['startdate'];
-				$datemonth = strtotime(date("Y-m-d", strtotime($date1)) . "+1 month");
+				$datemonth = strtotime(date("Y-m-d", strtotime($date1)) . "+'".$i1."' month");
 				echo "After adding one month: ".date('Y-m-d', $datemonth);
 				$duedate=date('Y-m-d', $datemonth);
 				$sql2="select * from App_Aux where App_Aux_field='TransactionType' and App_Aux_text='Regular Payment'";
