a25d63b802675e65604347e03f6749ff19565b5c
Dhiren <vaghani.dhiren.21@gmail.com>
2016-05-16 17:49:09 -0700

    activity

--- operation.php
+++ operation.php
@@ -1976,9 +1976,7 @@ $(document).ready(function(){
 				$result5=mysql_query($sql5);
 				$row5=mysql_fetch_array($result5);
 				$dueamount=$row3['App_Transactions_ShareAmount']-$row5['App_Transactions_ShareAmount'];
-				echo $row3['App_Transactions_ShareAmount'];
 				echo $row5['App_Transactions_ShareAmount'];
-				echo $dueamount;
 				?>
 				<input type="hidden" name="transids<?php echo $rowcnt ?>" class="transids<?php echo $rowcnt ?>" value="<?php echo $row3['App_Transactions_Id'] ?>"/>
 				<input type="hidden" name="transid<?php echo $rowcnt ?>" class="transid<?php echo $rowcnt ?>" />
