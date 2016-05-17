3dcfcd11cd167f867fbae4365cb03f89cd79b1e9
Dhiren <vaghani.dhiren.21@gmail.com>
2016-05-17 15:24:29 -0700

    activity

--- operation.php
+++ operation.php
@@ -1947,7 +1947,7 @@ $(document).ready(function(){
 			  $row=mysql_fetch_array($result);
 			  $sql3="select * from View_AgremTable aa INNER JOIN App_Credits ac ON ac.App_Credits_DebtorId = aa.App_Transactions_ClientID WHERE ac.App_Credits_AssignedTo ='".$_SESSION["logged_in_user"]["App_Users_ID"]."' and aa.App_Transactions_ShareStatus!='4' and aa.App_Transactions_ShareStatus!='6' and aa.App_Transactions_OperationID='".$row['App_Transactions_OperationID']."' and ac.App_Credits_BankOperNumber='".$row['App_Transactions_OperationID']."'";	
 			  $sql4="select * from View_AgremTable aa INNER JOIN App_Credits ac ON ac.App_Credits_DebtorId = aa.App_Transactions_ClientID WHERE ac.App_Credits_AssignedTo ='".$_SESSION["logged_in_user"]["App_Users_ID"]."' and aa.App_Transactions_ShareStatus!='4' and aa.App_Transactions_ShareStatus!='6' and aa.App_Transactions_OperationID='".$row['App_Transactions_OperationID']."' and ac.App_Credits_BankOperNumber='".$row['App_Transactions_OperationID']."' and MONTH(aa.App_Transactions_ShareDueDate)<='".Date("m")."' and YEAR(aa.App_Transactions_ShareDueDate)<='".Date("Y")."'";
-			  
+			  echo $sql4;
 			}
 				 $result3=mysql_query($sql3);
 				  $result4=mysql_query($sql4);
@@ -1955,6 +1955,7 @@ $(document).ready(function(){
 				  $num_row6=mysql_num_rows($result4);
 				  while($row4=mysql_fetch_array($result4)){ 
 				  $sql5="select * from App_TransHistory WHERE App_TransHistory_TransID = '".$row4['App_Transactions_Id']."'";
+				  echo $sql5;
 				$result5=mysql_query($sql5);
 				$num_row5=mysql_num_rows($result5);
 				
