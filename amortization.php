3df14bbadae49b5c682d29469828525415d03d40
Dhiren <vaghani.dhiren.21@gmail.com>
2016-05-30 16:36:05 +0530

    test done

--- amortization.php
+++ amortization.php
@@ -10,7 +10,7 @@
     <section class="content-header">
       <h1>
 	
-        Amortization  dhiren
+        Amortization
         <small>Supervisor</small>
       </h1>
       <ol class="breadcrumb">
