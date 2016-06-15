484d2e5f62cebda50bbe108a1009bb4ce9d33ec4
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-15 18:31:44 +0530

    left join check quer

--- operation.php
+++ operation.php
@@ -509,8 +509,7 @@ $(document).ready(function(){
                 <tbody>
 				<?php					
 					//$sql = "select * from App_Logs where App_Logs_OperationID = '".$_GET['operno']."'";
-					$sql = "select l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime
-									a.App_Aux_text as respuesta
+					$sql = "select DISTINCT l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,a.App_Aux_text as respuesta
 							from App_Logs l
 							left join App_Aux  a ON l.App_Logs_Answer = a.App_Aux_value
 							where App_Logs_OperationID = '".$_GET['operno']."'";
