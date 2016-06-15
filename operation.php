ce63f496612d441032f5f4c5aca872846cc23207
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-15 19:10:38 +0530

    inner join query check

--- operation.php
+++ operation.php
@@ -518,11 +518,20 @@ $(document).ready(function(){
 					$resultrespu=mysqli_fetch_array($resultrespu);
 					*/
 					
-					
+					/*
 					$sql = "select  l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,a.App_Aux_text as respuesta
 							from App_Logs l
 							left join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value
 							where App_Logs_OperationID = '".$_GET['operno']."'";
+							*/
+							
+					$sql="select *
+							from App_Logs l 
+							inner join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value 
+							inner join App_Aux a ON l.App_Logs_Contact = a.App_Aux_value
+							inner join App_Aux a ON l.App_Logs_Type = a.App_Aux_value							
+							where App_Logs_OperationID = '035866320130617000' and a.App_Aux_field = 'Answer' and a.App_Aux_field='Relation' and a.App_Aux_field='Tipo_Gestion' ";
+							
 					echo $sql; die();
 					
 					//$result=mysql_query($sql);
