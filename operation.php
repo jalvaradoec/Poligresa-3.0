a4b5d70cd06ba553a97a01003e3e36746c0091c6
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-15 16:02:43 +0530

    special cha display

--- operation.php
+++ operation.php
@@ -42,7 +42,7 @@ $(document).ready(function(){
         }
 });
 </script>
-<meta charset=utf-8>
+<meta charset="utf-8">
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
