95a5f1f5550fe60129bbcb3129fdefcddf68e86a
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-15 18:40:31 +0530

    left query check

--- operation.php
+++ operation.php
@@ -511,7 +511,7 @@ $(document).ready(function(){
 					//$sql = "select * from App_Logs where App_Logs_OperationID = '".$_GET['operno']."'";
 					$sql = "select DISTINCT l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,a.App_Aux_text as respuesta
 							from App_Logs l
-							left join App_Aux  a ON l.App_Logs_Answer = a.App_Aux_value
+							left join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value
 							where App_Logs_OperationID = '".$_GET['operno']."'";
 					echo $sql;
 					die();
