18f3a1b70722268b88d558bd93099683f90220bf
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-17 17:30:25 +0530

    update popup

--- operation.php
+++ operation.php
@@ -2140,27 +2140,138 @@ $(document).ready(function(){
 	  </div>
 	  </div>
 	  </div>
-	  
-		<!-- Update popup -->
-		<div class="modal fade" id="Oper_EditACtivities" role="dialog" style="width: 150%;margin-left: -24%;">
-			<div class="modal-dialog">
+	<div class="modal fade" id="Oper_EditACtivities" role="dialog" style="width: 150%;margin-left: -24%;">
+	 <div class="modal-dialog">
 		 
-				<!-- Modal content-->
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal">&times;</button>
-						<h4 class="modal-title">Activity</h4>
-					</div>
+     <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Activity</h4>
+        </div>
+		<?php
+				//$sql="select * from App_Tasks WHERE App_Tasks_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+			    $sql="select * from App_Tasks WHERE App_Task_ID =".$_GET['task_id'];
+				$result=mysql_query($sql);
+				$row=mysql_fetch_array($result);
+				$datetime=explode(" ",$row['App_Task_DueDateTime']);
+				$checked = ($row['App_Task_Status'] == 1) ? 'checked="checked' : '';
+				$sql1="select * from App_Users WHERE App_Users_ID =".$row["App_Tasks_AssignedTo"];
+				$result1=mysql_query($sql1);
+				$row1=mysql_fetch_array($result1);
+				$sql3="select * from App_Tasks ac INNER JOIN App_Clients ac1 ON ac.App_Task_DebtorID = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Tasks_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+				$result3=mysql_query($sql3);
+				$row3=mysql_fetch_array($result3);
+		?>
+		<form class="form-horizontal" method="post" action="">
+		<div class="modal-body">   
+		 <div class="box-body  no-padding md_box">
+		   <div class="col-lg-7 actv" style="width:65%">  
+			    <table class="activity_tbl" style="margin-top:0px">
+                <tbody>
+				<tr>
+                  <td class="deb_info_row">ID:</td>
+                  <td class="deb_info_row1"><?php echo $row['App_Task_DebtorID'] ?></td>          
+                </tr>
+			     <tr>
+                  <td class="deb_info_row">Name:</td>
+				  <td class="deb_info_row1"><?php echo $row3['App_Clients_FullName'] ?></td>          
+                </tr>
 				
-					<form class="form-horizontal" method="post" action="">
-						<div class="modal-body">   
-			
-						</div>
-						<div class="modal-footer">
-								<button type="submit" class="btn btn-info pull-left" name="updateactivity"><i class="fa fa-plus"></i>Update</button>
-							  <button type="button" class="btn btn-info" data-dismiss="modal"><i class="fa fa-reply"></i> Go Back</button>
-						</div>
-					</form>
+			 </tbody> 
+		     </table>
+			 </div>
+			 <div class="col-lg-4">
+			 <div class="activity_head1" style="margin-left:0px;color:gray">
+			 <h5><?php echo $row1['App_Users_fullname'] ?></h5>
+			 <h5><?php echo $row['App_Task_CreatedOn'] ?></h5>
+			 
+			 </div>
+			 </div>
+          </div>
+		  
+		  
+		<div class="box-body">
+		  <div class="col-lg-6" style="margin-left:-41px">
+		     
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-4 control-label">Type</label>
+                  <div class="col-sm-8">
+				  <select class="form-control" name="type" style="width:122%" required>
+                    <option value=""> -----------Select Type-----------</option>
+                    <?php
+					$ddl_secl = mysql_query("select * from App_Aux WHERE App_Aux_field = 'TaskType'");
+                    while ($r = mysql_fetch_assoc($ddl_secl)) {
+                           if($row['App_Task_TaskType']==$r['App_Aux_value']){
+							$selected1= 'selected="selected"';
+						}
+						else
+						{
+							$selected1='';
+						}
+                           echo "<option $selected1 value='$r[App_Aux_value]'> $r[App_Aux_text] </option>";
+                    }
+                    ?>
+                </select>
+                </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputPassword3" class="col-sm-4 control-label">Date</label>
+                  <div class="col-sm-8">
+                    <div class="input-group">
+                    <input type="date" id="dateselector" name="date" class="form-control" style="width: 150px;" value="<?php echo $datetime[0] ?>" required>
+				   <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                  </div>
+					</div>
+                  </div>
+                </div>
+                 <div class="form-group">
+                 <label for="inputPassword3" class="col-sm-4 control-label">Time</label>
+				 <div class="col-sm-8">
+				
+				 <input type="time" class="form-control" id="timeselector" name="time" value="<?php echo $datetime[1] ?>" required>
+                 </div>
+                </div>
+                 <div class="form-group">
+			  <label for="inputPassword3" class="col-sm-4 control-label">Status</label>
+			  <div class="col-sm-8">
+			   <div class="checkbox">
+                    <label>
+                      <input type="checkbox" name="status" <?php echo $checked; ?> value="1">
+                       Done
+                    </label>
+                  </div>
+              </div>
+              </div>
+            
+		  </div>
+		
+		  <div class="col-lg-6" style="margin-left:41px">    
+			   <h4>Task</h4>
+       
+			    <div class="form-group">
+                  <textarea class="form-control" rows="5" name="task" required ><?php echo $row['App_Task_Description'] ?></textarea>
+                </div>
+		
+		  </div>
+		   <div class="col-lg-12">    
+			   <h4>Outcome</h4>
+       
+			    <div class="form-group">
+                  <textarea class="form-control" rows="3" name="outcome" ><?php echo $row['App_Task_Outcome'] ?></textarea>
+                </div>
+		
+		  </div>
+		  </div>  
+		  
+     
+         </div>
+        <div class="modal-footer">
+        <button type="submit" class="btn btn-info pull-left" name="updateactivity"><i class="fa fa-plus"></i>Update</button>
+              <button type="button" class="btn btn-info" data-dismiss="modal"><i class="fa fa-reply"></i> Go Back</button>
+        </div>
+      </form>
 	  </div>
       
     </div>
