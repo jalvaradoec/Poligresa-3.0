3520fffe8f71a129d76924daa7a4abdc7e1970fd
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-17 12:33:31 +0530

    calculations popup create

--- operation.php
+++ operation.php
@@ -397,7 +397,7 @@ $(document).ready(function(){
 							<i class="fa fa-credit-card fa-5x fa_pay"></i>
 							<a href="#Oper_Transactions" data-toggle="modal" data-target="#Oper_Transactions"><h5 class="reg_pay">Setup Agreement</h5></a>				 				 
 							
-							<a href="#Oper_Transactions" data-toggle="modal" data-target="#Oper_Transactions"><h5 class="reg_pay">calculations</h5></a>
+							<a data-toggle="modal" data-target="#Oper_Calculations"><h5 class="reg_pay">calculations</h5></a>
 						</div>
 					</div>
 				</div>
@@ -1483,7 +1483,7 @@ $(document).ready(function(){
       
     </div>
   </div>  
-    <div class="modal fade" id="Oper_Transactions" role="dialog">
+  <div class="modal fade" id="Oper_Transactions" role="dialog">
 	 <div class="modal-dialog">
     
       <!-- Modal content-->
@@ -1665,6 +1665,149 @@ $(document).ready(function(){
       
     </div>
   </div>
+  
+	<!-- calculations popup -->
+	<div class="modal fade" id="Oper_Calculations" role="dialog">
+		<div class="modal-dialog">    
+		
+		<!-- Modal content-->
+		<div class="modal-content" style="width: 150%;margin-left: -24%;">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Calculations</h4>
+			</div>
+			<?php 
+				if(isset($_GET['operno'])){
+					$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber INNER JOIN App_Amortization ap ON ac.App_Credits_BankOperNumber = ap.App_Amortization_BankOperation WHERE  ap.App_Amortization_BankOperation =".$_GET["operno"];
+				}
+				else
+				{
+					$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber INNER JOIN App_Amortization ap ON ac.App_Credits_BankOperNumber = ap.App_Amortization_BankOperation WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+				}
+				$result=mysql_query($sql);
+				$row=mysql_fetch_array($result);
+				$interst=$row['App_Credits_BankTotalCredit']*0.18;
+						$initialdebt=$row['App_Credits_BankTotalCredit']+$interst;
+						$debt=$row['App_Credits_BankTotalCredit']+$interst-500;
+						$collectionfee=$initialdebt*0.2;
+						$currdebt=$debt+$collectionfee-150;
+				$sql2="select * from App_Agreement WHERE App_Agreement_DebtorID =".$row['App_Credits_DebtorId'];
+						$result2=mysql_query($sql2);
+						$row1=mysql_fetch_array($result2);
+						$checked = ($row['App_Task_Status'] == 1) ? 'checked="checked' : '';
+						$sql1="select * from App_Users WHERE App_Users_ID =".$_SESSION["logged_in_user"]["App_Users_ID"];
+						$result1=mysql_query($sql1);
+						$row2=mysql_fetch_array($result1);
+						$CreatedOn=explode(" ",$row1['App_Agreement_CreatedOn']);
+			?>
+			<form class="form-horizontal" method="post" action="">
+				<input type="hidden" name="hiddebt" class="hiddebt" value="<?php echo $debt ?>" />
+				<input type="hidden" name="operationid" value="<?php echo $row['App_Amortization_BankOperation'];?>" />
+				<input type="hidden" name="debtid" value="<?php echo $row['App_Credits_DebtorId'] ?>" />
+				
+				<div class="modal-body">   
+					<div class="box-body  no-padding md_box">
+						
+						
+					</div>
+					<div class="box-body">
+						<div class="col-lg-4">
+						
+							<div class="form-group">
+								<label for="inputPassword3" class="col-sm-4 control-label">Cur. Debt:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control curdebt" value="<?php echo number_format($currdebt, 2, '.', ''); ?>" name="curdebt" readonly >
+								</div>
+							</div>
+						
+							<div class="form-group">
+								<label for="inputPassword3" class="col-sm-4 control-label">Down Payment:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control dpayment" name="dpayment" onchange="setTwoNumberDecimal1()" >
+								</div>
+							</div>          
+						</div>
+						<div class="col-lg-4">					   
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-4 control-label">Discount:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control discount" name="discount" value="<?php echo number_format($collectionfee, 2, '.', ''); ?>" onchange="setTwoNumberDecimal()" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputPassword3" class="col-sm-4 control-label">Balance:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control balance2" name="balance2" readonly="">
+								</div>
+							</div>												  
+						</div>
+						
+						<div class="col-lg-4">             
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-4 control-label ">Balance:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control balance1" name="balance1" readonly="">
+								</div>
+							</div>
+							<div class="form-group">							
+								<label for="inputPassword3" class="col-sm-4 control-label">Interest:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control interest" name="interest" readonly="">
+								</div>
+							</div>		  
+						</div>  
+					</div>  
+					<div class="box-body">
+						<div class="col-lg-10" style="border:1px solid;margin-left: 70px;background-color: lightyellow;">						 
+							<div class="form-group" style="margin-top: 16px;">
+								<label for="inputPassword3" class="col-sm-5 control-label">The agreement is a downpayment of</label>
+								<div class="col-sm-4">
+									<input type="text" class="form-control dwnpymt" name="dwnpymt" readonly="">
+								</div>
+							</div>
+							
+							<div class="form-group">
+								<label for="inputPassword3" class="col-sm-2 control-label" style="margin-left: -17px;">then</label>
+								<div class="col-sm-2" style="margin-left: -21px;">
+									<input type="text" class="form-control shares" name="shares">
+								</div>
+								<label for="inputPassword3" class="col-sm-3 control-label" style="margin-left: -34px;">monthly payments of</label>
+								<div class="col-sm-4">
+									<input type="text" class="form-control monthpayment" name="monthpayment" readonly="">
+								</div>
+							</div>
+							
+							<div class="form-group">
+								<label for="inputPassword3" class="col-sm-4 control-label" style="margin-left: -24px;">and a last payments of</label>
+								<div class="col-sm-2" style="margin-left: -19px;">
+									<input type="text" class="form-control lastpayment" name="lastpayment" readonly="">
+								</div>
+								<label for="inputPassword3" class="col-sm-5 control-label" style="margin-left: -217px;">starting on</label>
+								<div class="col-sm-4">
+									<div class="input-group">
+										<input type="date" name="startdate" class="form-control">
+										<div class="input-group-addon">
+											<i class="fa fa-calendar"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		 
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-info pull-left" name="create"><i class="fa fa-plus"></i>Create Agreement</button>
+					<button type="button" class="btn btn-info" data-dismiss="modal"><i class="fa fa-reply"></i> Go Back</button>
+				</div>
+			</form>
+		</div>      
+	</div>	
+  <!-- end calculation -->
+  
+  
+  
+  
   <div class="modal fade" id="Edit_Transactions" role="dialog">
 	 <div class="modal-dialog">
     
