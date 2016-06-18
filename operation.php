b6213edf5494632f92ef216f14a0c7ed31e76ad2
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-18 16:17:49 +0530

    check design

--- operation.php
+++ operation.php
@@ -1883,22 +1883,22 @@ $(document).ready(function(){
           </div>
 		  
 		<div class="box-body">
-		  <div class="col-lg-4">
-		     
+		  <div class="col-lg-4">		     
                 <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-4 control-label">Type</label>
-                  <div class="col-sm-8">
-				  <select class="form-control" name="type" required>
-                    <option value=""> Select Type </option>
-                    <?php
-					$ddl_secl = mysql_query("select * from App_Aux WHERE App_Aux_field = 'TaskType'");
-                    while ($r = mysql_fetch_assoc($ddl_secl)) {
-                           echo "<option value='$r[App_Aux_value]'> $r[App_Aux_text] </option>";
-                    }
-                    ?>
-                </select>
+					<label for="inputEmail3" class="col-sm-4 control-label">Type</label>
+					<div class="col-sm-8">
+						<select class="form-control" name="type" required>
+							<option value=""> Select Type </option>
+							<?php
+								$ddl_secl = mysql_query("select * from App_Aux WHERE App_Aux_field = 'TaskType'");
+								while ($r = mysql_fetch_assoc($ddl_secl)) {
+									echo "<option value='$r[App_Aux_value]'> $r[App_Aux_text] </option>";
+								}
+							?>
+						</select>
+					</div>
                 </div>
-                </div>
+				
 				<div class="form-group">
                   <label for="inputEmail3" class="col-sm-4 control-label">Respuesta</label>
                   <div class="col-sm-8">
@@ -1914,6 +1914,7 @@ $(document).ready(function(){
                 </select>
                 </div>
                 </div>
+				
 				 <div class="form-group">
                   <label for="inputPassword3" class="col-sm-4 control-label">Comp/Abono:</label>
                   <div class="col-sm-8">
@@ -1931,7 +1932,7 @@ $(document).ready(function(){
                 </div>
 				-->
 		  </div>
-		<div class="col-lg-4">
+			<div class="col-lg-4">
 				<!--
                 <div class="form-group">
                   <label for="inputPassword3" class="col-sm-4 control-label">Date</label>
@@ -1945,8 +1946,24 @@ $(document).ready(function(){
                   </div>
                 </div>
 				-->
+				
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-4 control-label">Contacto</label>
+                  <div class="col-sm-8">
+				  <select class="form-control" name="contacto" style="width: 188px;" required>
+                    <option value=""> Select Contacto </option>
+					<?php
+						$ddl_secl_contact = mysql_query("select App_Aux_text,App_Aux_value from App_Aux WHERE App_Aux_field = 'Relation'");
+						while ($r = mysql_fetch_assoc($ddl_secl_contact)) {
+                        echo "<option value='$r[App_Aux_value]'> $r[App_Aux_text] </option>";
+                    }
+                    ?>
+                   
+                </select>
+                </div>
+                </div>
                 
-            <div class="form-group">
+				<div class="form-group">
                   <label for="inputEmail3" class="col-sm-4 control-label">Contacto</label>
                   <div class="col-sm-8">
 				  <select class="form-control" name="contacto" style="width: 188px;" required>
