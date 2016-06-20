d25085af48a3c23490b66ac5c1850710faa475f7
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-20 11:20:24 +0530

    query change

--- operation.php
+++ operation.php
@@ -534,7 +534,7 @@ $(document).ready(function(){
 							inner join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value 
 							inner join App_Aux aa ON l.App_Logs_Contact = aa.App_Aux_value
 							inner join App_Aux aaa ON l.App_Logs_Type = aaa.App_Aux_value							
-							where App_Logs_OperationID = '035866320130617000' and a.App_Aux_field = 'Answer' and aa.App_Aux_field='Relation' and aaa.App_Aux_field='Tipo_Gestion' ";																	
+							where App_Logs_OperationID = '".$_GET['operno']."' and a.App_Aux_field = 'Answer' and aa.App_Aux_field='Relation' and aaa.App_Aux_field='Tipo_Gestion' ";																	
 					$result=mysql_query($sql);
 					
 					while($row=mysql_fetch_array($result)){ 
