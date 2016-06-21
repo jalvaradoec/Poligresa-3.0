e9af96bc980fa6bbece0d5a31deaf8358375eb4a
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-21 15:50:26 +0530

    header duplication search remove

--- search.php
+++ search.php
@@ -290,9 +290,7 @@
     <!-- Main content -->
     <section class="content">
 		<div class="row">
-			<div class="col-md-12">
-				<h1>Search</h1>
-			</div>
+		<!--<div class="col-md-12"><h1>Search</h1></div>-->
 			
 			<div class="row">
 				<div class="col-md-12">
