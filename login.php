c99eeba8174456b61dece4a5c23c43ddaafa992c
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-22 10:23:57 +0530

    login check condition

--- login.php
+++ login.php
@@ -1,33 +1,34 @@
 <?php 
-session_start();
-include("web-config.php");
-?><!DOCTYPE html>
+	session_start();
+	include("web-config.php");
+?>
+<!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Poligresa 3.0 | Login</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.5 -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="dist/css/style.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+	<head>
+		<meta charset="utf-8">
+		  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		  <title>Poligresa 3.0 | Login</title>
+		  <!-- Tell the browser to be responsive to screen width -->
+		  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		  <!-- Bootstrap 3.3.5 -->
+		  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		  <!-- Font Awesome -->
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+		  <!-- Ionicons -->
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+		  <!-- Theme style -->
+		  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+		  <link rel="stylesheet" href="dist/css/style.css">
+		  <!-- iCheck -->
+		  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-</head>
+		  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		  <!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		  <![endif]-->
+	</head>
 <body class="hold-transition login-page">
 <div class="col-lg-12">
 <div class="login-logo">
@@ -107,41 +108,63 @@ include("web-config.php");
 </body>
 </html>
 <?php
-if(isset($_POST['signin'])){
-	$username=$_POST['username'];
-	$pwd=$_POST['pwd'];
-	$sql="select vl.*,au.App_Users_fullname,au.App_Users_email,au.App_Users_phone,au.App_Users_status,au.App_Users_memo from View_Logins vl LEFT JOIN App_Users au ON vl.App_Users_ID = au.App_Users_ID WHERE vl. App_Users_Username='".$username."' and vl.App_Users_password='".$pwd."'";
-  
-	$result=mysql_query($sql);
-	$num_row=mysql_num_rows($result);
-	if($num_row>0)
+if(!empty($_SESSION["username_admin"]))
+{
+	if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 9)
 	{
-		$_SESSION["logged_in_user"] = mysql_fetch_assoc($result);
-		$_SESSION['username_admin']=$username;
-		$_SESSION['pwd_admin']=$pwd;
-    $page = "";
-
-    if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 9)
-    {
-      $page = "sup_dashboard.php";
-    }
-    else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 5)
-    {
-      $page = "sup_dashboard.php";
-    }
-    else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 1)
-    {
-      $page = "oper_dashboard.php";
-    }
-		echo "<script>window.location.href='".$page."';</script>";
+	  $page = "sup_dashboard.php";
 	}
-	else
+	else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 5)
 	{
+	  $page = "sup_dashboard.php";
+	}
+	else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 1)
+	{
+	  $page = "oper_dashboard.php";
+	}
+		echo "<script>window.location.href='".$page."';</script>";
+}
+else
+{
+	
+	if(isset($_POST['signin']))
+	{
+		$username=$_POST['username'];
+		$pwd=$_POST['pwd'];
+		$sql="select vl.*,au.App_Users_fullname,au.App_Users_email,au.App_Users_phone,au.App_Users_status,au.App_Users_memo from View_Logins vl LEFT JOIN App_Users au ON vl.App_Users_ID = au.App_Users_ID WHERE vl. App_Users_Username='".$username."' and vl.App_Users_password='".$pwd."'";
+	  
+		$result=mysql_query($sql);
+		$num_row=mysql_num_rows($result);
+		if($num_row>0)
+		{
+			$_SESSION["logged_in_user"] = mysql_fetch_assoc($result);
+			$_SESSION['username_admin']=$username;
+			$_SESSION['pwd_admin']=$pwd;
+			$page = "";
+
+			if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 9)
+			{
+			  $page = "sup_dashboard.php";
+			}
+			else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 5)
+			{
+			  $page = "sup_dashboard.php";
+			}
+			else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 1)
+			{
+			  $page = "oper_dashboard.php";
+			}
+				echo "<script>window.location.href='".$page."';</script>";
+		}
+		else
+		{
 		?>
-		<script>
-		$('.login-box-msg').text('Please Enter Valid Username and Password!!');
-		</script>
+			<script>
+			$('.login-box-msg').text('Please Enter Valid Username and Password!!');
+			</script>
 		<?php
+		}
 	}
 }
+
 ?>
