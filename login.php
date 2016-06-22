3987ba4ea4cdd1f0a051994a5fc383d2ad928e1f
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-22 14:08:33 +0530

    check session

--- login.php
+++ login.php
@@ -2,19 +2,19 @@
 	session_start();
 	include("web-config.php");	
 	
-	if(!empty($_SESSION["logged_in_user"]["App_Users_SecurityLevel"]))
+	if(!$_SESSION['username_admin'])
 	{	
-		if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 9)
+		if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 9)
 		{
 		  $page = "sup_dashboard.php";
 		  echo "<script>window.location.href='".$page."';</script>";
 		}
-		else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 5)
+		else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 5)
 		{
 			$page = "sup_dashboard.php";
 			echo "<script>window.location.href='".$page."';</script>";
 		}
-		else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 1)
+		else if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] >= 1)
 		{
 			$page = "oper_dashboard.php";
 			echo "<script>window.location.href='".$page."';</script>";
