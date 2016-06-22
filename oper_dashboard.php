ff76b4b0f502e207f4d11a6abdd0b43701e0abef
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-22 15:30:33 +0530

    check condition

--- oper_dashboard.php
+++ oper_dashboard.php
@@ -2,7 +2,7 @@
 	session_start();
 	if(!empty($_SESSION['username_admin']))
 	{	
-		if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 1)
+		if($_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 10 || $_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 5 || $_SESSION["logged_in_user"]["App_Users_SecurityLevel"] == 1  )
 		{
 			include_once("header.php");
 			include_once("utils.php");
