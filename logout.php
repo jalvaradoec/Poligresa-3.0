637eeb7ca943aeb2c44df526637c20c37c0e769a
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-22 15:12:22 +0530

    logout as it is

--- logout.php
+++ logout.php
@@ -1,7 +1,5 @@
 <?php 
 session_start();
-unset($_SESSION['username_admin']);
-unset($_SESSION["logged_in_user"]["App_Users_SecurityLevel"]);
 session_destroy();
 header("Location: index.php");
 exit;
