cb8d84e2fca35e95d072f001fc75f172a10423e6
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 14:46:17 +0530

    change query

--- operation.php
+++ operation.php
@@ -40,8 +40,8 @@ h5
 			   </div>
 			   <?php			   
 				
-				$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
-				//$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_BankOperNumber ='".$_GET['operno']."' ";				
+				//$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+				$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_BankOperNumber ='".$_GET['operno']."' ";				
 				$result=mysql_query($sql);
 				$row=mysql_fetch_array($result);
 				$appcreditsdebid = $row['App_Credits_DebtorId'];
