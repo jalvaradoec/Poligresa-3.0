4e1e375c05ed8db152d5f28f60d3304690f8ef6f
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 14:19:37 +0530

    chaeck condiotion outer

--- operation.php
+++ operation.php
@@ -44,17 +44,21 @@ h5
 					echo  "TRUE";
 					//$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
 					$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_BankOperNumber ='".$_GET['operno']."' ";
-					$result=mysql_query($sql);
-					$row=mysql_fetch_array($result);
-					$appcreditsdebid = $row['App_Credits_DebtorId'];
+					//$result=mysql_query($sql);
+					//$row=mysql_fetch_array($result);
+					//$appcreditsdebid = $row['App_Credits_DebtorId'];
 				}
 				else{
 				echo  "FALSE";
 					$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
 					//$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_BankOperNumber ='".$_GET['operno']."' ";
+					//$row=mysql_fetch_array($result);
+					//$appcreditsdebid = $row['App_Credits_DebtorId'];
+				}
+				
+				$result=mysql_query($sql);
 					$row=mysql_fetch_array($result);
 					$appcreditsdebid = $row['App_Credits_DebtorId'];
-				}
 				
 			   ?>
             <div class="box-body no-padding">
