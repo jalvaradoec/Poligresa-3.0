f7af971e14d985e7e8748fa713b6d0db3385db97
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 11:19:59 +0530

    page back up

---
+++ tabs_configuration23-06-2016.php
@@ -0,0 +1,7 @@
+<ul class="nav nav-tabs">
+    <li <?php echo $general;?> ><a href="app_config.php?tab=general">General </a></li>
+    <li <?php echo $aux_info;?>><a href="app_config.php?tab=aux_info">Auxiliar information</a></li>
+    <li <?php echo $users;?>><a href="app_config.php?tab=users">Users</a></li>
+    <li <?php echo $data_load;?>><a href="app_config.php?tab=data_load">Data Load</a></li>
+    <li <?php echo $tools;?>><a href="app_config.php?tab=tools">Tools</a></li>
+</ul>
