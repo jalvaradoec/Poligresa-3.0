f96d171b4e3d6907a256649c94b5f6a5493c2dcd
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 13:53:47 +0530

    add condition query

--- operation.php
+++ operation.php
@@ -39,8 +39,8 @@ h5
 			   <h3>Debtor General Information</h3>
 			   </div>
 			   <?php			   
-				$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
-				//$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_BankOperNumber =".$_GET['operno'];				
+				//$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"];
+				$sql="select * from App_Credits ac INNER JOIN App_Clients ac1 ON ac.App_Credits_DebtorId = ac1.App_Clients_DebtorIdNumber WHERE  ac.App_Credits_BankOperNumber =".$_GET['operno'] AND ac.App_Credits_AssignedTo =".$_SESSION["logged_in_user"]["App_Users_ID"]";
 				$result=mysql_query($sql);
 				$row=mysql_fetch_array($result);
 				$appcreditsdebid = $row['App_Credits_DebtorId'];
