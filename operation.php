771662dd13e323cbc631cce5b08d97f94a7bbca9
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 15:37:03 +0530

    alert id

--- operation.php
+++ operation.php
@@ -3562,6 +3562,7 @@ $(document).on("click", ".addaddress", function () {
 });
 $(document).on("click", ".debtphone", function () {
 	var operno = $(this).attr("data-id");
+	alert(operno); return;
      window.location.href='operation.php?debtphone&operno=operno';
 	 
 });
