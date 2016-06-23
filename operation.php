9885a44fa467720aa1ef28d00f5593a2c3274a5f
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 10:26:33 +0530

    append row

--- operation.php
+++ operation.php
@@ -3923,7 +3923,7 @@ $(document).ready(function(){
 		
 	$('.moreactivity').click(function (){
 		alert ("More record found");
-		$('#applogsdata').append('<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td>');																
+		$('#applogsdata').append('<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>');																
 	});
 });
 </script>
