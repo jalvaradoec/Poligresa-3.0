e4e4f5335bd0cd9416d5028e0cda644d7381c132
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 19:14:44 +0530

    get final id

--- loadmore.php
+++ loadmore.php
@@ -3,16 +3,13 @@
 	if(isset($_POST['page'])):
 		$paged=$_POST['page'];
 		$resultsPerPage=3;
-		echo "rahul";
-		echo $_POST['operno']; die();
-		
 				
 		$sql="select l.App_Logs_Id,l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,l.App_Logs_Notes,a.App_Aux_text as respuesta,aa.App_Aux_text as contactto,aaa.App_Aux_text as telefono
 				from App_Logs l 
 				inner join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value 
 				inner join App_Aux aa ON l.App_Logs_Contact = aa.App_Aux_value
 				inner join App_Aux aaa ON l.App_Logs_Type = aaa.App_Aux_value							
-				where App_Logs_OperationID = '".$_GET['operno']."' and a.App_Aux_field = 'Answer' and aa.App_Aux_field='Relation' and aaa.App_Aux_field='Tipo_Gestion' order by App_Logs_Id DESC ";																										
+				where App_Logs_OperationID = '".$_POST['operno']."' and a.App_Aux_field = 'Answer' and aa.App_Aux_field='Relation' and aaa.App_Aux_field='Tipo_Gestion' order by App_Logs_Id DESC ";																										
 								
 		if($paged>0)
 		{
