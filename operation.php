9fe647f482e82ab4700915cfcf78ca7367e79f2d
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-23 10:31:57 +0530

    remove alert

--- operation.php
+++ operation.php
@@ -3921,8 +3921,7 @@ $(document).ready(function(){
 				$('#Oper_Aggrement').modal('show');	
         }
 		
-	$('.moreactivity').click(function (){
-		alert ("More record found");
+	$('.moreactivity').click(function (){		
 		$('#example2 tbody').append('<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>');																
 	});
 });
