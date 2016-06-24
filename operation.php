e03a6fc3dc5de52939043c221de343988d06282d
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-24 15:33:43 +0530

    get operno

--- operation.php
+++ operation.php
@@ -3999,7 +3999,10 @@ $(document).ready(function(){
 		
 		
 		function AllSearch(data)
-		{					
+		{		
+			var operno = "<?php echo $_GET['operno']?>";
+			alert (operno); return;
+			
 			var strurl = 'CustomerSearchPartial.php?fingreso=' + $('#fingreso').val() + '&respuesta=' + $('#respuesta').val() + '&contacto=' + $('#contacto').val() + '&telefono=' + $('#telefono').val() + '&valro=' +$('#valro').val() + '&fComp=' + $('#fComp').val() + '&comentarios=' +$('#comentarios').val(); 	
 						
 			$.ajax({
