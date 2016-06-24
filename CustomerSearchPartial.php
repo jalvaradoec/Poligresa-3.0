1fb506ad82712ea6e263ab141f8954922ad43b5d
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-24 17:30:47 +0530

    check query

--- CustomerSearchPartial.php
+++ CustomerSearchPartial.php
@@ -30,21 +30,30 @@
 	{
 		$condition .= "AND l.App_Logs_Notes='".$_GET['comentarios']."'";
 	}
+	
 	if($condition == ""){
 	
-	     $condition = "Rahul";
+	     $sql="select l.App_Logs_Id,l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,l.App_Logs_Notes,a.App_Aux_text as respuesta,aa.App_Aux_text as contactto,aaa.App_Aux_text as telefono
+							from App_Logs l 
+							inner join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value 
+							inner join App_Aux aa ON l.App_Logs_Contact = aa.App_Aux_value
+							inner join App_Aux aaa ON l.App_Logs_Type = aaa.App_Aux_value							
+							where App_Logs_OperationID = '".$_GET['operno']."' and a.App_Aux_field = 'Answer' and aa.App_Aux_field='Relation' and aaa.App_Aux_field='Tipo_Gestion' order by App_Logs_Id DESC  LIMIT 0,$resultsPerPage";																																							
 	}
-	
-	echo "Condition = ".$condition; die();
-		
-	
-	$sql="select l.App_Logs_Id,l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,l.App_Logs_Notes,a.App_Aux_text as respuesta,aa.App_Aux_text as contactto,aaa.App_Aux_text as telefono
+	else
+	{
+
+		$sql="select l.App_Logs_Id,l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,l.App_Logs_Notes,a.App_Aux_text as respuesta,aa.App_Aux_text as contactto,aaa.App_Aux_text as telefono
 							from App_Logs l 
 							inner join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value 
 							inner join App_Aux aa ON l.App_Logs_Contact = aa.App_Aux_value
 							inner join App_Aux aaa ON l.App_Logs_Type = aaa.App_Aux_value							
 							where 1=1 $condition AND  App_Logs_OperationID = '".$_GET['operno']."' and a.App_Aux_field = 'Answer' and aa.App_Aux_field='Relation' and aaa.App_Aux_field='Tipo_Gestion' order by App_Logs_Id DESC  LIMIT 0,$resultsPerPage";																																																				
-					
+				
+
+	}
+			
+		
 	$result=mysql_query($sql);
 	
 				
