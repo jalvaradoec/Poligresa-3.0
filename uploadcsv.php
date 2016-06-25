4aa30bd1a1a53608fcef5b89bd39586c008da254
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 13:35:32 +0530

    file upload

--- uploadcsv.php
+++ uploadcsv.php
@@ -1,8 +1,10 @@
 <?php
 	include_once("web-config.php");
 	echo "<br>Rahul<br>";
-	print_r($_POST); 
-	print_r($_FILES['file']['name']);
+	
+	echo "<pre>";
+	print_r($_FILES); 
+	//print_r($_FILES['file']['name']);
 	die();
 	
 	if (($_FILES["file"]["type"] == "text/csv"))
