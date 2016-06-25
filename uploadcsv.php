9214d625b08dda9dc8c45d380bc86b3255e2b81c
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 14:24:08 +0530

    echo data of file

--- uploadcsv.php
+++ uploadcsv.php
@@ -4,7 +4,11 @@
 	
 	echo "<pre>";
 	print_r($_FILES);
-	echo $_FILES["fileToUpload"]["name"];
+	echo "<br>".$_FILES["userfile"]["name"];
+	echo "<br>".$_FILES["userfile"]["type"];
+	echo "<br>".$_FILES["userfile"]["tmp_name"];
+	echo "<br>".$_FILES["userfile"]["size"];
+	
 	//print_r($_FILES['file']['name']);
 	die();
 	
