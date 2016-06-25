519a2c20822b3cfcec7390176d0f1a150989ac75
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 12:43:32 +0530

    like add

--- search.php
+++ search.php
@@ -330,7 +330,7 @@
 													OR acl.App_Clients_FullName like '%".$searchText."%'
 													OR ac.App_Credits_BankDueDate like '%".$searchText."%'
 													OR vos.App_Aux_text like '%".$searchText."%'													
-													OR acp.App_Phones_PhoneNumber ='".$searchText."'
+													OR acp.App_Phones_PhoneNumber like '%".$searchText."%'
 													GROUP BY  ac.App_Credits_BankOperNumber
 												";
 										}
