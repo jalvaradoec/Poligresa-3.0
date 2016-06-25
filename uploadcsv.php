d89d4ae60586f8c5d6cc61ae6a8aeaf23e1be596
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 13:33:33 +0530

    as it is

--- uploadcsv.php
+++ uploadcsv.php
@@ -2,7 +2,7 @@
 	include_once("web-config.php");
 	echo "<br>Rahul<br>";
 	print_r($_POST); 
-	print_r($_FILES);
+	print_r($_FILES['file']['name']);
 	die();
 	
 	if (($_FILES["file"]["type"] == "text/csv"))
