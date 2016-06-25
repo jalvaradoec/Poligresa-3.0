c020aa2e1f5da0e1266a8c4ea9fab4b6959b730d
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 12:23:33 +0530

    remove as it is

--- search.php
+++ search.php
@@ -312,7 +312,17 @@
 										$sqlCond = null;
 										if(!empty($searchText))
 										{
-									//OR act.App_Contacts_PhoneNumber like '%".$searchText."%' 	
+									//OR act.App_Contacts_PhoneNumber like '%".$searchText."%' 
+									/*
+									ac.App_Credits_BankOperNumber like '%".$searchText."%'
+													OR ac.App_Credits_DebtorId like '%".$searchText."%'
+													OR acl.App_Clients_FullName like '%".$searchText."%'
+													OR ac.App_Credits_BankDueDate like '%".$searchText."%'
+													OR vos.App_Aux_text like '%".$searchText."%'
+													OR act.App_Contacts_PhoneNumber = '".$searchText."'
+													OR acp.App_Phones_PhoneNumber ='".$searchText."'
+													GROUP BY  ac.App_Credits_BankOperNumber*/
+												";
 											$sqlCond .=" Where 
 													ac.App_Credits_BankOperNumber like '%".$searchText."%'
 													OR ac.App_Credits_DebtorId like '%".$searchText."%'
