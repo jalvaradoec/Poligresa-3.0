e9cddb9ef2e0dff7366148917319d546b189826a
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 14:46:11 +0530

    upload extention

--- uploadcsv.php
+++ uploadcsv.php
@@ -1,13 +1,22 @@
 <?php
 	include_once("web-config.php");
-	echo "<br>Rahul<br>";
+	
 	echo "<pre>";
 	print_r($_FILES);
 	echo "File name=".$_FILES["userfile"]["name"];
-	//echo "File Type=".$_FILES["userfile"]["type"];
 	
-	//die();
+	$allowed =  array('csv');
+	$filename = $_FILES['userfile']['name'];
+	$ext = pathinfo($filename, PATHINFO_EXTENSION);
+	if(!in_array($ext,$allowed) ) {
+		echo 'error';
+	}
+	else
+	{
+	 echo "<br>"."Upload file";
+	}
 	
+	die();
 	if (($_FILES["userfile"]["type"] == "text/csv"))
 	{
 		echo "File Type=".$_FILES["userfile"]["type"];
