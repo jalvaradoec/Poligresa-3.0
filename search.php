89e63cd085c99037058ad1ea9ef6db095a8bc866
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 12:19:54 +0530

    remove lke

--- search.php
+++ search.php
@@ -319,8 +319,8 @@
 													OR acl.App_Clients_FullName like '%".$searchText."%'
 													OR ac.App_Credits_BankDueDate like '%".$searchText."%'
 													OR vos.App_Aux_text like '%".$searchText."%'
-													OR act.App_Contacts_PhoneNumber = '".$searchText."'
-													OR acp.App_Phones_PhoneNumber like '%".$searchText."%'
+													OR act.App_Contacts_PhoneNumber = '"$searchText"'
+													OR acp.App_Phones_PhoneNumber ='"$searchText"'
 													GROUP BY  ac.App_Credits_BankOperNumber
 												";
 										}
