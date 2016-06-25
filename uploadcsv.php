d0b979ffdf3eb58ef3811908977a44ab67260f71
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 16:41:41 +0530

    echo

--- uploadcsv.php
+++ uploadcsv.php
@@ -19,6 +19,9 @@
 		if(move_uploaded_file($_FILES["userfile"]["tmp_name"], $target_file))
 		{
 			echo "The file ". basename( $_FILES["userfile"]["name"]). " has been uploaded.";
+			   //SampleCSVFile_2kb.csv 
+			
+			
 		}else 
 		{
 			echo "Sorry, there was an error uploading your file.";
