46c7cd5be79b6605beff52dc0c4291c392a88d75
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 12:15:50 +0530

    move likes

--- search.php
+++ search.php
@@ -319,7 +319,7 @@
 													OR acl.App_Clients_FullName like '%".$searchText."%'
 													OR ac.App_Credits_BankDueDate like '%".$searchText."%'
 													OR vos.App_Aux_text like '%".$searchText."%'
-													OR act.App_Contacts_PhoneNumber like '%".$searchText."%'
+													OR act.App_Contacts_PhoneNumber = '".$searchText."'
 													OR acp.App_Phones_PhoneNumber like '%".$searchText."%'
 													GROUP BY  ac.App_Credits_BankOperNumber
 												";
