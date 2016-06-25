0d104acd6718e7ca48e3652b671912b98afddc34
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-25 18:52:07 +0530

    print all records

--- uploadcsv.php
+++ uploadcsv.php
@@ -40,7 +40,7 @@
 	
 					$line_of_text = fgetcsv($file_handle, 1024);
 	
-					print $line_of_text[0] . $line_of_text[1]. $line_of_text[2] . "<BR>";
+					print $line_of_text[0] . $line_of_text[1]. $line_of_text[2] . $line_of_text[3] .$line_of_text[4] .$line_of_text[5] .$line_of_text[6] . "<BR>";
 	
 				}
 			
