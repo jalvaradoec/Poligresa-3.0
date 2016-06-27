122273152156f1403f143de9d5c735b8c6ccabd4
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-27 10:40:29 +0530

    count row

--- uploadcsv.php
+++ uploadcsv.php
@@ -26,10 +26,17 @@
 					while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
 					{
 						$num = count($data);
-						echo "<p> $num fields in line $row: <br /></p>\n";
+						
 						$row++;
 						for ($c=0; $c < $num; $c++) {
-							echo $data[$c] . "<br />\n";
+							if($c!=6)
+							{
+								echo $data[$c] . "<br />\n";
+							}
+							else{
+								exit;
+							}
+							
 						}
 						
 					}
