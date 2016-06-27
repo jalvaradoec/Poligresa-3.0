8d516597d2c29ecabbbe8cdfafc084931752690f
Dhiren <vaghani.dhiren.21@gmail.com>
2016-06-27 14:59:48 +0530

    as it is

--- operation.php
+++ operation.php
@@ -504,7 +504,7 @@ h5
 					$resultrespu=mysql_query($sqlrespu);
 					$resultrespu=mysqli_fetch_array($resultrespu);
 					*/															
-					echo $sql="select l.App_Logs_Id,l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,l.App_Logs_Notes,a.App_Aux_text as respuesta,aa.App_Aux_text as contactto,aaa.App_Aux_text as telefono
+					$sql="select l.App_Logs_Id,l.App_Logs_DateTime,l.App_Logs_Answer,l.App_Logs_Contact,l.App_Logs_Type,l.App_Logs_TransAmmount,l.App_Logs_TransDateTime,l.App_Logs_Notes,a.App_Aux_text as respuesta,aa.App_Aux_text as contactto,aaa.App_Aux_text as telefono
 							from App_Logs l 
 							inner join App_Aux a ON l.App_Logs_Answer = a.App_Aux_value 
 							inner join App_Aux aa ON l.App_Logs_Contact = aa.App_Aux_value
